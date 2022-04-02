ae3b5f1e34fc931084088fce77216d6fc67b2d75
Armi <armi.kurri@edu.omnia.fi>
2022-04-02 21:29:31 +0300

    testing my-new-branch

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+//Just testing my-new-branch
+
 const PokemonList = ({ pokemonList }) => {
   return (
     <div className="list-container">
