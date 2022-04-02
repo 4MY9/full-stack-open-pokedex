c678af8d6b25eb45133e3f6135daf1b1b6ed05fc
Armi <armi.kurri@edu.omnia.fi>
2022-04-02 22:01:44 +0300

    some feature added

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 //Just testing my-new-branch..
+//Adding some feature..
 
 const PokemonList = ({ pokemonList }) => {
   return (
