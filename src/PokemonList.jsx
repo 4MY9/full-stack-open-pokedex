84cbe124f7e392ff5226496f48799c4592f1fcb9
Armi <armi.kurri@edu.omnia.fi>
2022-04-03 18:41:21 +0300

    test

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-//testing
+//testing...
 const PokemonList = ({ pokemonList }) => {
   return (
     <div className="list-container">
